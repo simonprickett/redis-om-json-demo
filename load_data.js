cc12f72bdd5404ec6491730397faa67449d1d7e0
Simon Prickett <simon@redislabs.com>
2021-12-22 21:25:01 +0000

    Adds initial load and iteration over JSON file data.

--- load_data.js
+++ load_data.js
@@ -1,8 +1,16 @@
 // Load Data into Redis...
 import { Client, Repository } from 'redis-om';
+import { readFile } from 'fs/promises';
 
 async function loadAnimals() {
   console.log('TODO... load data!');
+
+  const animalData = JSON.parse(await readFile(new URL('./animal_data.json', import.meta.url)));
+
+  for (const animal of animalData.animals) {
+    const id = 'TODO';
+    console.log(`Loaded ${animal.name} the ${animal.species} as ${id}.`);
+  }
 }
 
 loadAnimals();
